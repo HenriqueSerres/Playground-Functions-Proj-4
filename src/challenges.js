dc44f2605b27a2feaee5c1aa7c390492ecfbdbff
Henrique Serres <henrique@gmail.com>
2021-10-18 15:35:54 -0300

    desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -8,12 +8,12 @@ function compareTrue(booleano1, booleano2) {
 
 // Desafio 2
 function calcArea(base, height) {
-  return (base * height) / 2
+  return (base * height) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split(' ');
 }
 
 // Desafio 4
